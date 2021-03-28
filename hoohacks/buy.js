9342fe9ca960cb11588868ce5eba7fea7e8296de
Rio-cyber <63576414+Rio-cyber@users.noreply.github.com>
2021-03-28 20:54:30 +0530

    initial commit

---
+++ hoohacks/buy.js
@@ -0,0 +1,15 @@
+
+var  abox = document.getElementsByClassName("box")[0];
+
+function ttb(){ 
+    var pic = 102;
+    document.getElementById('bigpic').style.display = "";
+    
+
+    }
+
+function ttb(){
+       abox.classList.remove("move-ltr");
+       abox.classList.remove("move");
+       abox.classList.toggle("move-ttb");
+}
